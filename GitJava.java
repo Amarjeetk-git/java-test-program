a49a92a107304f5c66379a5388a126e94348c73d
Amarjeetk-git <amarjeetk190198@gmail.com>
2026-03-26 18:23:09 +0530

    first commit

---
+++ GitJava.java
@@ -0,0 +1,17 @@
+import java.util.Properties;
+
+public class GitJava {
+  public static void myMethod() {
+    System.out.println("My first method excetued"); 
+    Properties props = System.getProperties();
+    System.out.println(props);
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Java Program statred.. ");
+    myMethod(); 
+    System.out.println("my new method of java for git hub");
+    
+  }
+
+}
